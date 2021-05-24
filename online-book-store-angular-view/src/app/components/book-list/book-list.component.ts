6cb46db4c3b2c2a34bc94bc4ca42b86a3a52fecb
vivekkumar333 <tiwari.vivekkumar333@gmail.com>
2021-05-25 00:52:09 +0530

    Search based on category has complated!

--- online-book-store-angular-view/src/app/components/book-list/book-list.component.ts
+++ online-book-store-angular-view/src/app/components/book-list/book-list.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Book } from 'src/app/common/book';
 import { BookService } from 'src/app/services/book.service';
-
+import {ActivatedRoute} from '@angular/router';
 
 @Component({
   selector: 'app-book-list',
@@ -11,14 +11,29 @@ import { BookService } from 'src/app/services/book.service';
 export class BookListComponent implements OnInit {
 
   books: Book[];
-  constructor(private _bookService: BookService) { }
+  currentCategoryId : number;
+
+  constructor(private _bookService: BookService,
+    private _activetedRoute: ActivatedRoute) { }
 
   ngOnInit(): void {
-    this.listBooks();
+    this._activetedRoute.paramMap.subscribe(() =>{
+        this.listBooks();
+    })
   }
 
   listBooks(){
-    this._bookService.getBooks().subscribe(
+    const hasCategoryId : boolean = this._activetedRoute.snapshot.paramMap.has('id');
+    if(hasCategoryId){
+      this.currentCategoryId = +this._activetedRoute.snapshot.paramMap.get('id');
+    }else{
+      this.currentCategoryId = 1;
+    }
+
+
+
+
+    this._bookService.getBooks(this.currentCategoryId).subscribe(
       data =>{
         this.books = data;
       }
--- online-book-store-angular-view/src/app/services/book.service.ts
+++ online-book-store-angular-view/src/app/services/book.service.ts
@@ -13,8 +13,10 @@ export class BookService {
 
   constructor(private HttpClient : HttpClient) { }
 
-  getBooks() : Observable<Book[]>{
-    return this.HttpClient.get<GetResponseBooks>(this.baseUrl).pipe(
+  getBooks(theCategoryId : number) : Observable<Book[]>{
+    const searchUrl = `${this.baseUrl}/search/categoryId?id=${theCategoryId}`
+
+    return this.HttpClient.get<GetResponseBooks>(searchUrl).pipe(
       map(response => response._embedded.books)
     );
   }
